98565c7301700e9bcce381a7cc2d2264e7ee4bb3
Madhuban83 <87249424+Madhuban83@users.noreply.github.com>
2021-07-10 16:43:47 -0700

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,26 @@
+var box
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,10,10)
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyDown(RIGHT_ARROW))
+  {
+    box.x = box.x + 3
+  }
+  if(keyDown(LEFT_ARROW))
+  {
+    box.x = box.x - 3
+  }
+  drawSprites()
+
+}
+
+
+
+
